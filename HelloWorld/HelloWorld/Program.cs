b48dd3f5917bbeb8ba99b0774c65ed4eb12eeff2
Jakob Flodman <jakob.flodman@flodmans.se>
2022-03-11 15:51:33 +0100

    #8 Changed

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello!");
+            Console.WriteLine("Hello!!!");
         }
     }
 }
